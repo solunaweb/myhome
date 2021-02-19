b8d2f9d39059198abfbc85dd99930f39bc06a053
leejinmyung <fuzo0701@naver.com>
2021-02-19 11:26:03 +0900

    Srping Validation

---
+++ src/main/java/com/gocoder/myhome/controller/AccountController.java
@@ -0,0 +1,34 @@
+package com.gocoder.myhome.controller;
+
+import com.gocoder.myhome.model.User;
+import com.gocoder.myhome.repository.UserRepository;
+import com.gocoder.myhome.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
+    public String login(){
+        return "account/login";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "account/register";
+    }
+
+    @PostMapping("/register")
+    public String register(User user){
+        userService.save(user);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/gocoder/myhome/service/UserService.java
@@ -0,0 +1,32 @@
+package com.gocoder.myhome.service;
+
+import com.gocoder.myhome.model.Role;
+import com.gocoder.myhome.model.User;
+import com.gocoder.myhome.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User save(User user){
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        user.setEnabled(true);
+
+        Role role = new Role();
+        role.setId(1l);
+
+        user.getRoles().add(role);
+
+
+        return userRepository.save(user);
+    }
+}
